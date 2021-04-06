b28feb8233841e28169653a99a0df7a44f59b9ca
Georkis <georkis.santiesteban@gmail.com>
2021-04-06 18:29:30 -0400

    Agregando routind

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -18,7 +18,6 @@ use Symfony\Component\Validator\Constraints as Assert;
 /**
  * @ORM\Entity(repositoryClass=ProductRepository::class)
  * @UniqueEntity(fields={"url"})
- * @UniqueEntity(fields={"slug"})
  */
 class Product
 {
